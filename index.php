865d0453d659b8019fd914f0474ed6445073e33f
root <root@almarin.atica.um.es>
2012-05-03 13:44:09 +0200

    include horde skeleton files

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Skeleton index script.
+ *
+ * Copyright 2007-2012 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @author Your Name <you@example.com>
+ */
+
+require dirname(__FILE__) . '/list.php';
---
+++ lib/Exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Base exception class for Skeleton.
+ *
+ * Copyright 2010-2012 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @author   Your name <you@example.com>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/gpl GPL
+ * @package  Skeleton
+ */
+class Skeleton_Exception extends Horde_Exception_Wrapped
+{
+}
